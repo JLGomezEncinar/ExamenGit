d4ac59b1940e588e26f8b1b7609df5f27fe5584d
Santy Cierco <santidisturbios@gmail.com>
2024-11-14 17:26:33 +0100

    First commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Este programa muestra la tabla de multiplicar del 5");
+        int numero = 5;
+        int contador = 1;
+
+        while (contador <= 10) {
+            System.out.println(numero + " x " + contador + " = " + (numero * contador));
+
+        }
+
+    }
+}
